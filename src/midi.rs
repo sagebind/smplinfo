c87ffdaae5c3f81fbcc07ef1a60f232a7eb2c415
Stephen M. Coakley <me@stephencoakley.com>
2021-06-10 18:38:19 -0500

    Initial commit

---
+++ src/midi.rs
@@ -0,0 +1,77 @@
+use std::{fmt, str::FromStr};
+
+const NOTE_NAMES: &'static [&'static str] = &["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+
+#[derive(Copy, Clone, Debug, Default, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Note(u8);
+
+impl From<u8> for Note {
+    fn from(value: u8) -> Self {
+        Self(value)
+    }
+}
+
+impl From<Note> for u8 {
+    fn from(note: Note) -> Self {
+        note.0
+    }
+}
+
+impl FromStr for Note {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if let Ok(number) = s.parse::<u8>() {
+            return Ok(number.into());
+        }
+
+        for (offset, name) in NOTE_NAMES.into_iter().enumerate() {
+            if let Some(suffix) = s.strip_prefix(name) {
+                if let Ok(number) = suffix.parse::<u8>() {
+                    return Ok(Note::from(((number as usize + 1) * NOTE_NAMES.len() + offset) as u8));
+                }
+            }
+        }
+
+        Err("invalid MIDI note".into())
+    }
+}
+
+impl fmt::Display for Note {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if self.0 >= 12 {
+            let index = (self.0 - 12) as usize;
+            f.write_str(NOTE_NAMES[index % NOTE_NAMES.len()])?;
+
+            ((self.0 as usize - NOTE_NAMES.len()) / NOTE_NAMES.len()).fmt(f)
+        } else {
+            self.0.fmt(f)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse() {
+        assert_eq!(Note::from_str("0").unwrap(), Note::from(0));
+        assert_eq!(Note::from_str("51").unwrap(), Note::from(51));
+        assert_eq!(Note::from_str("A0").unwrap(), Note::from(21));
+        assert_eq!(Note::from_str("C1").unwrap(), Note::from(24));
+    }
+
+    #[test]
+    fn display_name() {
+        assert_eq!(Note::from(0).to_string(), "0");
+        assert_eq!(Note::from(11).to_string(), "11");
+        assert_eq!(Note::from(12).to_string(), "C0");
+        assert_eq!(Note::from(20).to_string(), "G#0");
+        assert_eq!(Note::from(21).to_string(), "A0");
+        assert_eq!(Note::from(22).to_string(), "A#0");
+        assert_eq!(Note::from(23).to_string(), "B0");
+        assert_eq!(Note::from(24).to_string(), "C1");
+        assert_eq!(Note::from(127).to_string(), "G9");
+    }
+}
---
+++ src/wav.rs
@@ -0,0 +1,118 @@
+//! WAV format reading and writing routines.
+
+use crate::midi;
+use anyhow::{bail, Result};
+use riff::Chunk;
+use std::io::{Read, Seek, SeekFrom, Write};
+
+pub struct Wav<F> {
+    file: F,
+    header: Chunk,
+}
+
+impl<F: Read + Seek> Wav<F> {
+    pub fn new(mut file: F) -> Result<Self> {
+        let header = Chunk::read(&mut file, 0)?;
+
+        if header.read_type(&mut file)?.as_str() != "WAVE" {
+            bail!("not a WAV file");
+        }
+
+        Ok(Self { file, header })
+    }
+
+    pub fn get_sampler_chunk(&mut self) -> Result<Option<SamplerChunk>> {
+        if let Some(offset) = self.find_chunk_offset("smpl")? {
+            self.file.seek(SeekFrom::Start(offset))?;
+
+            Ok(Some(SamplerChunk::read(&mut self.file)?))
+        } else {
+            Ok(None)
+        }
+    }
+
+    fn find_chunk_offset(&mut self, id: &str) -> Result<Option<u64>> {
+        let chunk = Chunk::read(&mut self.file, 0)?;
+
+        for child in chunk.iter(&mut self.file) {
+            if child.id().as_str() == id {
+                return Ok(Some(child.offset()));
+            }
+        }
+
+        Ok(None)
+    }
+}
+
+impl<F: Read + Seek + Write> Wav<F> {
+    pub fn update_sampler_chunk(&mut self, f: impl FnOnce(&mut SamplerChunk)) -> Result<()> {
+        if let Some(offset) = self.find_chunk_offset("smpl")? {
+            self.file.seek(SeekFrom::Start(offset))?;
+            let mut chunk = SamplerChunk::read(&mut self.file)?;
+            f(&mut chunk);
+
+            self.file.seek(SeekFrom::Start(offset))?;
+            self.file.write_all(chunk.as_ref())?;
+        } else {
+            let mut chunk = SamplerChunk::default();
+            f(&mut chunk);
+
+            self.file.seek(SeekFrom::End(0))?;
+            self.file.write_all(chunk.as_ref())?;
+
+            let new_size = self.header.len() + chunk.as_ref().len() as u32;
+            self.file.seek(SeekFrom::Start(4))?;
+            self.file.write_all(&new_size.to_le_bytes())?;
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub struct SamplerChunk {
+    header: [u8; 44],
+}
+
+impl Default for SamplerChunk {
+    fn default() -> Self {
+        let mut header = [0; 44];
+
+        &mut header[..4].copy_from_slice(Self::ID);
+        header[5] = 36;
+
+        Self { header }
+    }
+}
+
+impl SamplerChunk {
+    const ID: &'static [u8] = b"smpl";
+
+    fn read<R>(mut reader: R) -> Result<Self>
+    where
+        R: Read + Seek,
+    {
+        let mut header = [0; 44];
+        reader.read_exact(&mut header)?;
+
+        if &header[..4] != Self::ID {
+            bail!("invalid smpl chunk");
+        }
+
+        Ok(Self { header })
+    }
+
+    pub fn midi_unity_note(&self) -> midi::Note {
+        self.header[0x14].into()
+    }
+
+    pub fn set_midi_unity_note(&mut self, note: midi::Note) {
+        self.header[0x14] = note.into();
+    }
+}
+
+impl AsRef<[u8]> for SamplerChunk {
+    fn as_ref(&self) -> &[u8] {
+        &self.header
+    }
+}
